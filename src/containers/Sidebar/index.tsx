420e1597a1f6af42e09cc609aa46e863e1e2fbbb
Kayky Dion <kayky.dion@gmail.com>
2024-04-09 19:41:09 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from "../../components/Avatar";
+import Paragrafo from "../../components/Paragrafo";
 import Titulo from "../../components/Titulo";
+import { BotaoTema, Descricao, SidebarContainer } from "./styles";
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/KaykyDion.png" />
-    <Titulo fontSize={20}>Kayky Dion</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Kayky Dion</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        KaykyDion
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 );
 
